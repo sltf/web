5444e51f8a167b56200ee87b938c8e428e3859c2
Pulasthi Mahawithana <pulasthi7@gmail.com>
2020-09-09 15:57:52 +0530

    Updates

---
+++ precache-manifest.5c5b398c1b52e101abc701140570a963.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829235a2324fbda84806a8c8bb6cd223",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "660bcc2e460b1c864008",
+    "url": "/web/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "72d4b6caaeb5eb886521",
+    "url": "/web/static/css/main.76b02867.chunk.css"
+  },
+  {
+    "revision": "660bcc2e460b1c864008",
+    "url": "/web/static/js/2.2532a547.chunk.js"
+  },
+  {
+    "revision": "b65ea0a01c563efcdb7edae091a9eae1",
+    "url": "/web/static/js/2.2532a547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d4b6caaeb5eb886521",
+    "url": "/web/static/js/main.5bbd490c.chunk.js"
+  },
+  {
+    "revision": "68d8844d16396f59cafa",
+    "url": "/web/static/js/runtime-main.3e09b273.js"
+  },
+  {
+    "revision": "14580db22987b2c3ae22c54c8c5c9640",
+    "url": "/web/static/media/playstore.14580db2.png"
+  }
+]);
